69e9203881d37b8a25fc1b7d9c2359744f8e1879
Henry Matsumoto <henryamatsu@gmail.com>
2026-03-27 19:03:08 -0400

    week 8

---
+++ week 8 - binary trees/invert_binary_tree.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if root == None:
+            return
+
+        stack = [root]
+
+        while len(stack) > 0:
+            node = stack.pop()
+        
+            if node.left != None:
+                stack.append(node.left)
+            if node.right != None:
+                stack.append(node.right)
+
+            temp = node.left
+            node.left = node.right
+            node.right = temp
+
+        return root
